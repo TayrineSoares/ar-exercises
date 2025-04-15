087579c7f80f8804b64ac61782ae7b31f9ff7f03
TayrineSoares <tayrinecristina@hotmail.com>
2025-04-15 15:55:29 -0400

    Exercise 3: Delete the third store

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -5,4 +5,11 @@ require_relative './exercise_2'
 puts "Exercise 3"
 puts "----------"
 
-# Your code goes here ...
+# Load the third store
+@store3 = Store.find(3)
+
+# Delete it from the database
+@store3.destroy
+
+# Output the current number of stores to confirm
+puts "Number of stores remaining: #{Store.count}"
